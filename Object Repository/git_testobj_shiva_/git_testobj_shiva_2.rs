60c9a012cb9b7583e463a2e33327948d867c4e1c
shivakandur <kandurshivakumar06@gmail.com>
2024-05-07 15:51:57 +0530

    Add project to git

---
+++ Object Repository/git_testobj_shiva_/git_testobj_shiva_2.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>git_testobj_shiva_2</name>
+   <tag></tag>
+   <elementGuidId>b3bb5201-b78b-43be-aa9d-f845700d31bd</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
